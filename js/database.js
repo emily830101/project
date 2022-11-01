3c164215ab81f977a10fadc6bf6dc861f7fc3825
emily830101 <s603860725@gmail.com>
2022-11-01 23:57:36 +0800

    20221101個專第一次demo

---
+++ js/database.js
@@ -0,0 +1,19 @@
+var rooms = 
+[{
+    id: "roomA",
+    name: "典雅日系雙人房",
+    description: "暖陽灑落的明亮窗景，舒適的臥房和寬敞的露臺，如空中花園般將綠意山景收入眼底，適合情侶、夫妻享受悠閒假期",
+    price: "TWD 11,400",
+    img: "../image/roomA1.png",
+    capacity: 2,
+    unavailableDates: ["2022-11-15"]
+},
+{   id: "roomB",
+    name: "豪華海景雙人房",
+    description: "超大坪數的臨海衛浴，飽覽太平洋風光，白天與黑夜展現不同樣貌，不時可見大船入港，星光熠熠，令人目不暇給",
+    price: "TWD 16,800",
+    img: "../image/roomB1.png",
+    capacity: 2,
+    unavailableDates: []
+}
+];
